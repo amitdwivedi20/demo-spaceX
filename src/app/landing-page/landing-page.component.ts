55973b3e372bfa8f201f331bdfc50d4a7ab5ceba
Amit Kumar6 <amit.kumar6@ihsmarkit.com>
2020-11-01 21:25:52 +0530

    landing page changes

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, UrlSerializer } from '@angular/router';
 import { FilterModel } from '../filters/filter.model';
 import { SpacexService } from '../service/spacex.service';
-import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-landing-page',
+  selector: 'landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.css']
 })
@@ -12,63 +12,45 @@ export class LandingPageComponent implements OnInit {
 
   public filter: FilterModel;
   public items;
-  constructor(
-    private location: Location,
-    private spaceXservice: SpacexService) {
+  constructor(private spaceXservice: SpacexService, private router: Router, private route: ActivatedRoute, private serializer: UrlSerializer) {
     this.filter = new FilterModel();
   }
 
   ngOnInit(): void {
     this.getlandingPageItems(this.createQuery(this.filter));
-    this.updateUrl(this.createQuery(this.filter));
+    this.updateUrl(this.filter)
     this.getFilter();
   }
 
 
-  private getFilter(): void {
+  private getFilter() {
     this.spaceXservice.getMessageForFilter().subscribe(res => {
-      this.updateUrl(this.createQuery(res.value));
+      this.updateUrl(res.value);
       this.getlandingPageItems(this.createQuery(res.value));
     });
   }
 
-  public getlandingPageItems(params): void {
+  public getlandingPageItems(params) {
     this.spaceXservice.getAll(params).subscribe((res) => {
       this.items = [...res];
     }, (error) => {
-      console.log(error);
-    });
+      console.log(error)
+    })
   }
 
-  public updateUrl(queryParams): void {
-    this.location.replaceState(queryParams);
-  }
-
-  public createQuery(params): string {
-    return this.createUri(params);
-  }
-
-
-  private getQuery(val): string {
-    let txt = '';
-    val.split('').forEach((element, index) => {
-      if (element === element.toUpperCase()) {
-        if (index !== 0) {
-          element = '_' + element.toLowerCase();
-        }
-      }
-
-      txt += element;
-    });
-    return txt;
+  public updateUrl(queryParams) {
+    this.router.navigate(
+      [],
+      {
+        relativeTo: this.route,
+        queryParams: queryParams,
+        queryParamsHandling: 'merge'
+      });
   }
 
 
-  createUri(params): string {
-    let uri = '/?';
-    for (const param in Object.keys(params)) {
-      uri += this.getQuery(param) + '=' + params[param] + '&';
-    }
-    return uri.substring(0, uri.length - 1);
+  public createQuery(params) {
+    let tree = this.router.createUrlTree([], { queryParams: params });
+    return this.serializer.serialize(tree);
   }
 }
